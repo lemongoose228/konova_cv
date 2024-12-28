fc6c591f420de4e50264a4c5e9c239230740a72b
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-12-28 10:04:53 +0800

    Update main.py

--- trajectory/main.py
+++ trajectory/main.py
@@ -3,12 +3,14 @@ import os
 import matplotlib.pyplot as plt
 import cv2
 
+# Функция для вычисления евклидова расстояния между двумя точками p1 и p2
 def compute_distance(p1, p2):
     return np.linalg.norm(np.array(p1) - np.array(p2))
 
 
 def load_images_from_directory(directory):
     files = os.listdir(directory)
+    # Сортируем файлы по числовой части имени 
     files = sorted(files, key=lambda x: int(x.split('_')[1].split('.')[0]))
 
     images = []
@@ -20,6 +22,7 @@ def load_images_from_directory(directory):
     return images
 
 def extract_contour_coordinates(files):
+    # Функция для извлечения координат контуров из списка изображений
     trajectories = {"obj1": [], "obj2": [], "obj3": []}
 
     for i, image in enumerate(files):
@@ -40,9 +43,12 @@ def extract_contour_coordinates(files):
                 trajectory = trajectories[key]
 
                 if trajectory:
-                    last_point = trajectory[-1]
+                    last_point = trajectory[-1] # Получаем последнюю точку траектории
+                    # Находим ближайшую точку из текущих координат к последней точке траектории
                     next_point = min(coords, key=lambda p2: compute_distance(last_point, p2), default=None)
 
+                    
+                    # Если найдена ближайшая точка то добавляем её в траекторию и удаляем из списка доступных кординат
                     if next_point is not None:
                         trajectory.append(next_point)
                         coords.remove(next_point)
